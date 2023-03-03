ec49c8a8b10709462d046bb6e9f10370d0f6f660
20hdinh <HarryDinh2629@gmail.com>
2023-03-02 21:03:49 -0800

    pseudocode and helper for minimax, not functional

--- tictactoe.py
+++ tictactoe.py
@@ -65,3 +65,33 @@ def minimax(board):
     Returns the optimal action for the current player on the board.
     """
     raise NotImplementedError
+
+# note: minimax will directly call terminal(), score(), player(), actions()
+# minimax will indirectly call result() from calling actions()
+# minimax will need all other functions implemented before working
+
+# def minimax(board):
+    # return minimaxHelper(board, alpha, beta)
+
+# def minimaxHelper(board, alpha, beta)
+    # base case: leaf node, just returns 1, 0, or -1
+    # if terminal(board)
+        # return score(board)
+    
+    # recursive case
+    # if !player(board)  # we are the ai and want to win; max value
+        # maxEval = -infinity
+        # for each branch of actions(board)
+            # eval = minimax(branch, alpha, beta, false)
+            # maxEval = max(maxEval, eval)
+            # alpha =  max(alpha, eval)
+            # if beta <= alpha
+                # break
+        # return maxEval
+
+    # else  # we are viewing the player; player wants min value for us
+        # for each branch of actions(board)
+            # eval = minimax(branch, alpha, beta, true)
+            # minEval = min(minEval, eval)
+            # beta = min(beta, eval)
+        # return minEval
