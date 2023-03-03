1c5d9cf57e92be443234fe674be3a91dcbd68bc6
20hdinh <HarryDinh2629@gmail.com>
2023-03-02 21:53:41 -0800

    convert from pseudocode to python

--- tictactoe.py
+++ tictactoe.py
@@ -64,34 +64,44 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
-
 # note: minimax will directly call terminal(), score(), player(), actions()
 # minimax will indirectly call result() from calling actions()
 # minimax will need all other functions implemented before working
 
-# def minimax(board):
-    # return minimaxHelper(board, alpha, beta)
+    # the ai is the current player() so we know what letter 'X' or 'O' it is
+    # if ai is 'X' we want the final score to be 1 (max)
+    # if ai is '0' we want the final score to be -1 (min)
+    # also lazy: infinity = 9999, -infinity = -9999
 
-# def minimaxHelper(board, alpha, beta)
+    if player(board) == 'X':
+        return minimaxHelper(board, -9999, +9999, 1)
+    else:
+        return minimaxHelper(board, -9999, 9999, 0)
+
+def minimaxHelper(board, alpha, beta, maximizingPlayer):
+
     # base case: leaf node, just returns 1, 0, or -1
-    # if terminal(board)
-        # return score(board)
+    if terminal(board):
+        return score(board)
     
-    # recursive case
-    # if !player(board)  # we are the ai and want to win; max value
-        # maxEval = -infinity
-        # for each branch of actions(board)
-            # eval = minimaxHelper(branch, alpha, beta)
-            # maxEval = max(maxEval, eval)
-            # alpha =  max(alpha, eval)
-            # if beta <= alpha
-                # break
-        # return maxEval
+    # recursive cases
 
-    # else  # we are viewing the player; player wants min value for us
-        # for each branch of actions(board)
-            # eval = minimaxHelper(branch, alpha, beta)
-            # minEval = min(minEval, eval)
-            # beta = min(beta, eval)
-        # return minEval
+    if maximizingPlayer:
+        maxEval = -9999
+        for branch in actions(board):
+            eval = minimaxHelper(result(board, branch), alpha, beta)
+            maxEval = max(maxEval, eval)
+            alpha =  max(alpha, eval)
+            if beta <= alpha:
+                break
+        return maxEval
+
+    else:
+        minEval = 9999
+        for branch in actions(board):
+            eval = minimaxHelper(result(board, branch), alpha, beta)
+            minEval = min(minEval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return minEval
